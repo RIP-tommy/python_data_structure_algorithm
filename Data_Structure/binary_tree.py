33c96de7ef34d60c28a03f2e6a059a3e559bc858
lordtomi0325 <sungminchoidev@gmail.com>
2021-07-05 22:37:21 +0900

    add Data_structure Part

---
+++ Data_Structure/binary_tree.py
@@ -0,0 +1,41 @@
+class Node:
+    def __init__(self, data, left = None, right = None):
+        self.data = data
+        self.left = left
+        self.right = right
+
+class Binary_Tree:
+    def __init__(self):
+        self.root = None
+        self.current = None
+        self.left = None
+        self.right = None
+    
+    def insert(self, data):
+        if not self.root:
+            self.root = Node(data)
+            self.current = self.root
+        else:
+            while True:
+                if self.current.data is data:
+                    break
+                if data < self.current.data and not self.left:
+                    self.left = Node(data)
+                elif data < self.current.data and self.left:
+                    self.current = self.left
+                elif data > self.current.data and not self.right:
+                    self.right = Node(data)
+                elif data > self.current.data and self.right:
+                    self.current = self.right
+
+    def in_order(self):
+        print(self.root.data)
+
+node = (50, 15, 62, 5, 20, 58, 91, 3, 8, 37, 60, 24)
+
+tree = Binary_Tree()
+
+for i in node:
+    tree.insert(i)
+
+tree.in_order()
